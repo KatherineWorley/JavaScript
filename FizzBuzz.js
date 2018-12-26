0a10d4f9f3bb7a157b3acee62b19e0bb199a965e
Katherine Worley <kdesign216@yahoo.com>
2018-12-26 10:39:04 -0500

    fizzBuzz baby

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -7,3 +7,65 @@ function fizzBuzz(num){
 	}
 }
 fizzBuzz(20)
+
+function fizzBuzz(){
+	for(var i=1;i<=100;i++){
+		if(i%5 === 0 && i%3 === 0){
+			print('FizzBuzz');
+		} else if(i%3 === 0){
+			print('Fizz');
+		} else if(i%5 === 0){
+			print('Buzz');
+		} else {
+			print(i);
+		}
+	}
+}
+
+function fizzBuzz(){
+	for(var i=1;i<=100;i++){
+		if(i%3 === 0){
+			print('Fizz');
+		} 
+		if(i%5 === 0){
+			print('Buzz');
+		} 
+		if(i%3 !== 0 && i%5 !== 0) {
+			print(i);
+		}
+	}
+}
+
+function fizzBuzz(){
+	var output;
+	for(var i=1;i<=100;i++){
+		output = '';
+		if(i%3 === 0){
+			output+='Fizz';
+		} 
+		if(i%5 === 0){
+			output+='Buzz';
+		} 
+		if(i%3 !== 0 && i%5 !== 0) {
+			output+=i;
+		}
+		print(output);
+	}
+}
+
+function fizzBuzz(){
+	var output;
+	for(var i=1;i<=100;i++){
+		output = '';
+		if(i%3 === 0){
+			output+='Fizz';
+		} 
+		if(i%5 === 0){
+			output+='Buzz';
+		} 
+		if(output === '') {
+			output+=i;
+		}
+		print(output);
+	}
+}
