c8b0f429e6bb5bc5fc5f620296533b581e234e61
Katherine Worley <kdesign216@yahoo.com>
2018-12-26 10:24:17 -0500

    working on fizzbuzz drills

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,10 +1,9 @@
 function fizzBuzz(num){
 	for(let i = 1; i <- num; i++){
 		if(i % 3 === 0 && i % 5 === 0) console.log("Fizz Buzz");
-		else if(i % 3 === 0 ) console.log()
-
+		else if(i % 3 === 0 ) console.log("Fizz");
+		else if(i % 5 === 0 ) console.log("Buzz");
+		else console.log(i);
 	}
-
-
-
 }
+fizzBuzz(20)
