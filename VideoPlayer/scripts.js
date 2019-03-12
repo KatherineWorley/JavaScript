87fce91ae52165cbf5aa4701bfde86e68578b33b
Katherine Worley <kdesign216@yahoo.com>
2019-03-12 08:20:09 -0400

    adding toggle play button

--- VideoPlayer/scripts.js
+++ VideoPlayer/scripts.js
@@ -8,16 +8,21 @@ const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
-
 // build our functions 
 function togglePlay() {
-	if(video.paused) {
-		video.play();
-	} else {
-		video.pause();
-	}
+	const method = video.paused ? 'play' : 'paused';
+	video[method]();
 }
 
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  console.log(icon);
+  toggle.textContent = icon;
+}
 
 // hook up the event listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('click', togglePlay);
 
