a69e9547447b2c93e99edd63523abe61e79506f9
Katherine Worley <kdesign216@yahoo.com>
2019-03-12 08:08:43 -0400

    adding scripts to video tut

---
+++ VideoPlayer/scripts.js
@@ -0,0 +1,12 @@
+//get our elements
+
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+
+
+
+// build our functions 
+
+
+// hook up the event listeners
+
