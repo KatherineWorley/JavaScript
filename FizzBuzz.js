06a26ea16767fdc125f3e09f63dc857672693af5
Katherine Worley <kdesign216@yahoo.com>
2019-03-23 16:17:06 -0400

    touching on fizzbuzz

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -6,66 +6,84 @@ function fizzBuzz1(num){
 		else console.log(i);
 	}
 }
-fizzBuzz1(20)
+// fizzBuzz1(20)
 
-function fizzBuzz2(){
-	for(var i=1;i<=100;i++){
-		if(i%5 === 0 && i%3 === 0){
-			print('FizzBuzz');
-		} else if(i%3 === 0){
-			print('Fizz');
-		} else if(i%5 === 0){
-			print('Buzz');
-		} else {
-			print(i);
-		}
+// function fizzBuzz2(){
+// 	for(var i=1;i<=100;i++){
+// 		if(i%5 === 0 && i%3 === 0){
+// 			print('FizzBuzz');
+// 		} else if(i%3 === 0){
+// 			print('Fizz');
+// 		} else if(i%5 === 0){
+// 			print('Buzz');
+// 		} else {
+// 			print(i);
+// 		}
+// 	}
+// }
+
+// function fizzBuzz3(){
+// 	for(var i=1;i<=100;i++){
+// 		if(i%3 === 0){
+// 			print('Fizz');
+// 		} 
+// 		if(i%5 === 0){
+// 			print('Buzz');
+// 		} 
+// 		if(i%3 !== 0 && i%5 !== 0) {
+// 			print(i);
+// 		}
+// 	}
+// }
+
+// function fizzBuzz4(){
+// 	var output;
+// 	for(var i=1;i<=100;i++){
+// 		output = '';
+// 		if(i%3 === 0){
+// 			output+='Fizz';
+// 		} 
+// 		if(i%5 === 0){
+// 			output+='Buzz';
+// 		} 
+// 		if(i%3 !== 0 && i%5 !== 0) {
+// 			output+=i;
+// 		}
+// 		print(output);
+// 	}
+// }
+
+// function fizzBuzz5(){
+// 	var output;
+// 	for(var i=1;i<=100;i++){
+// 		output = '';
+// 		if(i%3 === 0){
+// 			output+='Fizz';
+// 		} 
+// 		if(i%5 === 0){
+// 			output+='Buzz';
+// 		} 
+// 		if(output === '') {
+// 			output+=i;
+// 		}
+// 		print(output);
+// 	}
+// }
+
+function fizzBuzz1(num){
+	for(let i = 1; i <- num; i++){
+		if(i % 3 === 0 && i % 5 === 0) console.log("Fizz Buzz");
+		else if(i % 3 === 0 ) console.log("Fizz");
+		else if(i % 5 === 0 ) console.log("Buzz");
+		else console.log(i);
 	}
 }
 
-function fizzBuzz3(){
-	for(var i=1;i<=100;i++){
-		if(i%3 === 0){
-			print('Fizz');
-		} 
-		if(i%5 === 0){
-			print('Buzz');
-		} 
-		if(i%3 !== 0 && i%5 !== 0) {
-			print(i);
-		}
-	}
-}
-
-function fizzBuzz4(){
-	var output;
-	for(var i=1;i<=100;i++){
-		output = '';
-		if(i%3 === 0){
-			output+='Fizz';
-		} 
-		if(i%5 === 0){
-			output+='Buzz';
-		} 
-		if(i%3 !== 0 && i%5 !== 0) {
-			output+=i;
-		}
-		print(output);
-	}
-}
-
-function fizzBuzz5(){
-	var output;
-	for(var i=1;i<=100;i++){
-		output = '';
-		if(i%3 === 0){
-			output+='Fizz';
-		} 
-		if(i%5 === 0){
-			output+='Buzz';
-		} 
-		if(output === '') {
-			output+=i;
-		}
-		print(output);
+function fizzBuzz2(num){
+	for(let i=1; i <- num; i++){
+		if(i % 3 === 0 && i % 5 === 0) console.log("Fizz Buzz");
+			else if(i % 3 === 0) console.log("Fizz");
+			else if(i % 5 === 0) console.log("Buzz"):
+			else console.log(i);
 	}
 }
