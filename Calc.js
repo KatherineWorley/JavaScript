b888649477d762dad5ee4e59dff850a989970545
Katherine Worley <kdesign216@yahoo.com>
2019-04-18 09:51:08 -0400

    working on js

--- Calc.js
+++ Calc.js
@@ -2,32 +2,30 @@ const calculator = document.querySelector(‘.calculator’)
 const keys = calculator.querySelector(‘.calculator__keys’)
 keys.addEventListener(‘click’, e => {
  if (e.target.matches(‘button’)) {
-   // Do something
+   if (!action) {
+	  console.log('number key!')
+	}
+
+	if (
+	  action === 'add' ||
+	  action === 'subtract' ||
+	  action === 'multiply' ||
+	  action === 'divide'
+	) {
+	  console.log('operator key!')
+	}
+
+	if (action === 'decimal') {
+	  console.log('decimal key!')
+	}
+	if (action === 'clear') {
+	  console.log('clear key!')
+	}
+	if (action === 'calculate') {
+	  console.log('equal key!')
+	}
  }
 })
 
 const key = e.target
 const action = key.dataset.action
-
-if (!action) {
-  console.log('number key!')
-}
-
-if (
-  action === 'add' ||
-  action === 'subtract' ||
-  action === 'multiply' ||
-  action === 'divide'
-) {
-  console.log('operator key!')
-}
-
-if (action === 'decimal') {
-  console.log('decimal key!')
-}
-if (action === 'clear') {
-  console.log('clear key!')
-}
-if (action === 'calculate') {
-  console.log('equal key!')
-}
